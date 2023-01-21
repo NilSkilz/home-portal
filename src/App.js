1a36c516c043d14dc877077b5a52e1e93b19f021
Rob Stokes <rob.stokes@superdry.com>
2023-01-21 12:52:41 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mt-5">
+          <h1 style={{ fontWeight: 500 }}>cracky.co.uk</h1>
+        </div>
+        <div className="row mb-5">
+          <div>Application Portal</div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 col-lg-4">
+            <Card
+              title="Plex"
+              subtitle="Movies & TV Shows"
+              url="plex.cracky.co.uk"
+              logo="/plex_logo.png"
+            />
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <Card
+              title="Overseerr"
+              subtitle="Request Management"
+              url="overseerr.cracky.co.uk"
+              logo="/overseerr_logo.png"
+            />
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <Card
+              title="Home Assistant"
+              subtitle="Home Automation & IOT"
+              url="ha.cracky.co.uk"
+              logo="/ha_logo.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div style={{
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+        textAlign: 'center',
+        marginBottom: '100px'
+      }}>
+        <a href='/status/cracky'>Status Page</a></div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Card = ({ title, subtitle, url, logo }) => {
+    return (
+        <a href="test.com">
+            <div className="row" style={{ textAlign: 'right', marginBottom: '100px' }}>
+                <div className="col-4"><img style={{ height: '80px', marginTop: '20px' }} src={logo}></img></div>
+                <div className="col-8"><div
+                    style={{
+                        fontWeight: "500",
+                        fontSize: "38px",
+                        textAlign: "left",
+                        // width: '300px'
+                    }}
+                >
+                    {title}
+                </div>
+                    <div
+                        style={{
+                            fontWeight: "500",
+                            fontSize: "14px",
+                            textAlign: "left",
+                        }}
+                    >
+                        {subtitle}
+                    </div>
+                    <div
+                        style={{
+                            fontWeight: "200",
+                            fontSize: "14px",
+                            textAlign: "left",
+                        }}
+                    >
+                        {url}
+                    </div></div>
+
+            </div>
+
+        </a>
+    );
+};
+
+export default Card;
